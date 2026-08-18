d2e8aa81c73daa47345cd7a76c51894d899cec20
Elian Manzueta <elianmanzueta@protonmail.com>
2026-08-18 16:32:32 -0400

    Fish

--- stow/.config/fish/conf.d/path.fish
+++ stow/.config/fish/conf.d/path.fish
@@ -1,8 +1,6 @@
-switch (uname)
-        fish_add_path "~/.local/bin"
-        fish_add_path "~/.config/emacs/bin/"
-        fish_add_path "~/.cargo/bin/"
-end
+fish_add_path "~/.local/bin"
+fish_add_path "~/.config/emacs/bin/"
+fish_add_path "~/.cargo/bin/"
 
 if type -q trash-put
     abbr rm trash-put
