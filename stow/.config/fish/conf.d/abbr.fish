9ddf03d99fdd56334a49841b7e961f43ed791d8c
Elian Manzueta <elianmanzueta@protonmail.com>
2026-08-22 22:59:04 -0400

    Fish

--- stow/.config/fish/conf.d/abbr.fish
+++ stow/.config/fish/conf.d/abbr.fish
@@ -20,20 +20,31 @@ if type -q zoxide
     abbr -a cd z
 end
 
+if type -q systemctl
+    abbr -a sysctl "systemctl"
+    abbr -a usysctl "systemctl --user"
+
+    abbr -a jctl "journalctl"
+    abbr -a ujctl "journalctl --user"
+end
+
 abbr -a gst git status
 abbr -a gcsm git commit --signoff --message
 abbr -a gp git push
 abbr -a ga git add
 abbr -a gc git clone
 
-if type -q emacsclient
+if systemctl --user is-active emacs.service
+    abbr -a ed "systemctl --user start emacs"
+    abbr -a red "systemctl --user restart emacs"
+    abbr -a killemacs "systemctl --user kill emacs"
+
     abbr -a em "emacsclient -c"
     abbr -a et "emacsclient -nw"
 
+    abbr -a ff vterm_cmd find-file .
+    abbr -a orgidp git commit --signoff --message ".orgids"
+else
     abbr -a killemacs "emacsclient -e \"(kill-emacs)"\"
     abbr -a ed "emacs --daemon"
-
-    abbr -a ff vterm_cmd find-file .
-
-    abbr -a orgidp git commit --signoff --message ".orgids"
 end
