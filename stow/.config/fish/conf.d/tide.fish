671d614ff6bfb9ae738263ef2a7d41b42d93e7bb
Elian Manzueta <elianmanzueta@protonmail.com>
2026-08-28 10:05:13 -0400

    Tide config

---
+++ stow/.config/fish/conf.d/tide.fish
@@ -0,0 +1,12 @@
+# [prompt]
+set --universal tide_left_prompt_items context $tide_left_prompt_items
+set --universal tide_right_prompt_items status cmd_duration jobs direnv bun node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+
+# [character]
+set --universal tide_character_icon ">"
+set --universal tide_character_vi_icon_default "<"
+set --universal tide_character_vi_icon_visual "v"
+set --universal tide_character_vi_icon_replace "x"
+
+# [python]
+set --universal tide_python_icon "py"
