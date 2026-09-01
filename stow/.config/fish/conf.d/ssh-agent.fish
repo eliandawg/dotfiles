6d180e9780258b3f6bec784eb04a7771e41a6dbd
Elian Manzueta <elianmanzueta@protonmail.com>
2026-09-01 14:44:35 -0400

    add check for systemd in ssh-agent.fish
    Signed-off-by: Elian Manzueta <elianmanzueta@protonmail.com>

--- stow/.config/fish/conf.d/ssh-agent.fish
+++ stow/.config/fish/conf.d/ssh-agent.fish
@@ -1,3 +1,5 @@
-if systemctl --user is-active ssh-agent > /dev/null
-    set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+if type -q systemctl
+    if systemctl --user is-active ssh-agent >/dev/null
+        set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    end
 end
